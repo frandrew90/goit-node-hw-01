1b6e701d5bf6ecfe1e83ada8e2fa6c2b9f27e8a1
frandrew90 <frandrewjs@gmail.com>
2021-11-10 15:07:16 +0200

    made the third step of HW3

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const {
   addContact,
 } = require("./contacts.js");
 
-listContacts().then((data) => console.log(data));
+// listContacts().then((data) => console.log(data));
 // getContactById(2);
 // removeContact("2e54f4c3-42d3-4ce8-8bc1-3cc4af6aedfc");
 // addContact("AAAAA", "BBBBBBBBB", "CCCCCCCCC");
